bf110207393587c568c6a52262b0949688b9b53c
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 00:35:23 -0400

    Expose optional input applier from public include

--- src/pypilot_nmea0183_connector.hpp
+++ src/pypilot_nmea0183_connector.hpp
@@ -5,4 +5,5 @@
 #include "pypilot_nmea0183_connector/parse_helpers.hpp"
 #include "pypilot_nmea0183_connector/stream_parser.hpp"
 #include "pypilot_nmea0183_connector/apply_sentence_pypilot.hpp"
+#include "pypilot_nmea0183_connector/optional_inputs.hpp"
 #include "pypilot_nmea0183_connector/format_sentence.hpp"
