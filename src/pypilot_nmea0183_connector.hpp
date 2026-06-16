4a45e07fb5c58c0478ad97fdf4d534020fe9c1a1
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-16 00:08:14 -0400

    Use pypilot-exact sentence applier in public include

--- src/pypilot_nmea0183_connector.hpp
+++ src/pypilot_nmea0183_connector.hpp
@@ -4,5 +4,5 @@
 #include "pypilot_nmea0183_connector/sentence.hpp"
 #include "pypilot_nmea0183_connector/parse_helpers.hpp"
 #include "pypilot_nmea0183_connector/stream_parser.hpp"
-#include "pypilot_nmea0183_connector/apply_sentence.hpp"
+#include "pypilot_nmea0183_connector/apply_sentence_pypilot.hpp"
 #include "pypilot_nmea0183_connector/format_sentence.hpp"
