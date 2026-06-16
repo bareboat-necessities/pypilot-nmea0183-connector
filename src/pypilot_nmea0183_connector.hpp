c335b2e7c49eb75ae2ac34fdb8b10c28206010c0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 23:04:46 -0400

    Add public connector include

---
+++ src/pypilot_nmea0183_connector.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "pypilot_nmea0183_connector/checksum.hpp"
+#include "pypilot_nmea0183_connector/sentence.hpp"
+#include "pypilot_nmea0183_connector/parse_helpers.hpp"
+#include "pypilot_nmea0183_connector/stream_parser.hpp"
+#include "pypilot_nmea0183_connector/apply_sentence.hpp"
+#include "pypilot_nmea0183_connector/format_sentence.hpp"
