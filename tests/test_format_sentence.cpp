de3e104b6c9404a2c9cf41cd60919254c312fab3
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-15 23:33:49 -0400

    Test generic RSA formatter

--- tests/test_format_sentence.cpp
+++ tests/test_format_sentence.cpp
@@ -13,5 +13,8 @@ int main() {
     make_mwv(line, sizeof(line), 45.0f, 12.3f, true);
     assert(verify_nmea_checksum(line));
     assert(std::strstr(line, "$IIMWV,45.0,R,12.3,N,A*") == line);
+
+    make_rsa(line, sizeof(line), 2.5f);
+    assert(std::strcmp(line, "$IIRSA,2.5,A,,V*7E\r\n") == 0);
     return 0;
 }
