a5280a07f1682a9870a76196cb44a6f0a6c9f969
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 13:06:39 -0400

    Expose first class NMEA input surface

--- src/pypilot_nmea0183_connector.hpp
+++ src/pypilot_nmea0183_connector.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include "pypilot_nmea0183_connector/checksum.hpp"
 #include "pypilot_nmea0183_connector/sentence.hpp"
 #include "pypilot_nmea0183_connector/parse_helpers.hpp"
 #include "pypilot_nmea0183_connector/stream_parser.hpp"
 #include "pypilot_nmea0183_connector/apply_sentence_pypilot.hpp"
-#include "pypilot_nmea0183_connector/optional_inputs.hpp"
 #include "pypilot_nmea0183_connector/format_sentence.hpp"
