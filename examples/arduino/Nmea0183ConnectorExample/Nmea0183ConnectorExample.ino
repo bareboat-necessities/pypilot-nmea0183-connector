454ceeb1d00728dfdd80a4b062f9fda684a44f9f
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 17:19:56 -0400

    Update Arduino example for renamed NMEA connector

--- examples/arduino/Nmea0183ConnectorExample/Nmea0183ConnectorExample.ino
+++ examples/arduino/Nmea0183ConnectorExample/Nmea0183ConnectorExample.ino
@@ -3,8 +3,8 @@
 #include <pypilot_nmea0183_connector.hpp>
 
 pypilot_data_model::DataModel<> model;
-pypilot_nmea0183_connector::Nmea0183StreamParser parser;
-pypilot_nmea0183_connector::Nmea0183Connector<> connector;
+nmea0183_connector::Nmea0183StreamParser parser;
+nmea0183_connector::Nmea0183RxConnector<> connector;
 
 void setup() {
   Serial.begin(115200);
@@ -13,7 +13,7 @@ void setup() {
 
 void loop() {
   while (Serial1.available()) {
-    pypilot_nmea0183_connector::NmeaSentence sentence;
+    nmea0183_connector::NmeaSentence sentence;
     if (parser.push((char)Serial1.read(), sentence)) {
       connector.apply_sentence(sentence, model, micros());
     }
