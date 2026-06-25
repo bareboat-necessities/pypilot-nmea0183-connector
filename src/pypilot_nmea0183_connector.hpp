513c9163f3129ada176e7e01bb20b91004681d84
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 13:41:59 -0400

    Use sentence formatter public header

--- src/pypilot_nmea0183_connector.hpp
+++ src/pypilot_nmea0183_connector.hpp
@@ -3,4 +3,4 @@
 #include "pypilot_nmea0183_connector/nmea0183_helpers.hpp"
 #include "pypilot_nmea0183_connector/sentence_parser.hpp"
 #include "pypilot_nmea0183_connector/apply_sentence_pypilot.hpp"
-#include "pypilot_nmea0183_connector/format_sentence.hpp"
+#include "pypilot_nmea0183_connector/sentence_formatter.hpp"
