f785fe8141db6e2f32e89b320c4ef25a891c2314
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2026-06-25 14:15:19 -0400

    Expose NMEA RX and TX connectors

--- src/pypilot_nmea0183_connector.hpp
+++ src/pypilot_nmea0183_connector.hpp
@@ -2,5 +2,5 @@
 
 #include "pypilot_nmea0183_connector/nmea0183_helpers.hpp"
 #include "pypilot_nmea0183_connector/sentence_parser.hpp"
-#include "pypilot_nmea0183_connector/apply_sentence_pypilot.hpp"
-#include "pypilot_nmea0183_connector/sentence_formatter.hpp"
+#include "pypilot_nmea0183_connector/nmea_rx_connector.hpp"
+#include "pypilot_nmea0183_connector/nmea_tx_connector.hpp"
